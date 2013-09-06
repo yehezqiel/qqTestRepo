a06a54f86647c8f4bbe44d640da8626428231839
Kiki <Kiki@Kiki-PC>
2013-09-06 22:21:27 +0700

    commit after tambah test 3

---
+++ JavaApplication3/src/qq/test/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qq.test;
+
+/**
+ *
+ * @author Kiki
+ */
+public class NewClass {
+    
+}
